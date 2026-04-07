a849fd1d594ae832216c66dfd86c349293a43b88
Thiago Costa <thiagocosta.bsb@gmail.com>
2026-04-07 05:25:14 -0300

    ResponseEntity (12/29)

--- src/main/java/br/com/thiagocosta/learning/userRelated/controller/ApiController.java
+++ src/main/java/br/com/thiagocosta/learning/userRelated/controller/ApiController.java
@@ -8,6 +8,8 @@ import br.com.thiagocosta.learning.userRelated.model.UserModel;
 import org.springframework.web.bind.annotation.RequestBody;
 import br.com.thiagocosta.learning.userRelated.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 // @Controller Usado para aplicações MVC tradicionais (que retornam Views/HTML como Thymeleaf ou JSP).
 @RestController // Usado para APIs REST (que retornam JSON ou XML). Ele é, na verdade, uma combinação de @Controller + @ResponseBody
@@ -26,16 +28,18 @@ public class ApiController {
         PATCH - Alterar somente uma parte da info/dado
     */
     @PostMapping("/create")
-    public UserModel createUser(@RequestBody UserModel userModel){
+    public ResponseEntity createUser(@RequestBody UserModel userModel){
         var user = this.userRepository.findByUserName(userModel.getUserName());
 
         if(user != null){
             System.out.println("User already found!");
-            return null;
+            //Mensagem de erro
+            // Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
